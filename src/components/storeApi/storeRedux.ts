8c6ed0597e2c613e76dd2fc383bb0d604f47e881
poniNa4as <145863283+poniNa4as@users.noreply.github.com>
2024-01-11 11:11:44 +0100

    new

--- src/components/storeApi/storeRedux.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { storeApi } from "./storeApi";
-
-export const storeRedux = configureStore({
-    reducer: {
-        [storeApi.reducerPath]: storeApi.reducer
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(storeApi.middleware)
-})
